2bfa39d405284dd18b56bc35e6b2b4c96748dd71
FabijanC <fabijan.corak@gmail.com>
2021-11-17 16:18:13 +0100

    test

--- test/plain/check.sh
+++ test/plain/check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+echo "DEBUG: delete this line; it's here just have some change for a new commit"
 npx hardhat starknet-compile
 npx hardhat starknet-deploy --starknet-network alpha
 npx hardhat test
